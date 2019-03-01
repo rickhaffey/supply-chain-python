f39b44467553d871f7ff634c394ffe30370debd5
Rick Haffey <rhaffey@fanatics.com>
2019-03-01 08:46:28 -0600

    week 7 - pp 2 (part 2)

--- SC1x/week07_pp02.py
+++ SC1x/week07_pp02.py
@@ -1,28 +1,32 @@
 import pandas as pd
 
-demand = pd.read_csv('~/Desktop/edX/SC1x/data/W07_PP_Sodatastic24_data.csv')
-demand['probability'] = demand['probability'] / 100.0
-demand['CumProb'] = demand['probability'].cumsum()
+
+def run_scenario(price):
+    path = '~/Desktop/edX/SC1x/data/W07_PP_Sodatastic{}_data.csv'.format(price)
+    data = pd.read_csv(path)
+    data['probability'] = data['probability'] / 100.0
+    data['CumProb'] = data['probability'].cumsum()
+
+    def weighted_profit(row, ordered, price):
+        cost = 6.75
+        demand = row['cases']
+        sold = min(demand, ordered)
+        prob = row['probability']
+        return (price * sold - cost * ordered) * prob
+
+    o_cols = []
+    for i in range(1, data['cases'].max() + 1):
+        col_name = 'o_{}'.format(i)
+        o_cols.append(col_name)
+        data[col_name] = data.apply(weighted_profit, axis=1, args=[i, price])
+
+    totals = data.loc[:, o_cols].sum()
+    max_value = totals.max()
+
+    print("-" * 20)
+    print("scenario: price=${}".format(price))
+    print(totals[totals == max_value])
 
 
-def weighted_profit(row, ordered):
-    price = 24
-    cost = 6.75
-    demand = row['cases']
-    sold = min(demand, ordered)
-    prob = row['probability']
-    return (price * sold - cost * ordered) * prob
-
-
-o_cols = []
-for i in range(1, demand['cases'].max() + 1):
-    col_name = 'o_{}'.format(i)
-    o_cols.append(col_name)
-    demand[col_name] = demand.apply(weighted_profit, axis=1, args=[i])
-
-totals = demand.loc[:, o_cols].sum()
-max_value = totals.max()
-
-print(totals[totals == max_value])
-
-# >>> o_16    188.112
+run_scenario(24)
+run_scenario(20)
